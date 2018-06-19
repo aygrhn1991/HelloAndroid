8c7ec2cc9c31285f6e5d07e2eda6194d8f190444
chenyufeng <chenyufeng@DESKTOP-0D1OC2O>
2018-06-19 16:20:29 +0800

    123

--- app/src/main/java/com/fenglingtime/www/helloandroid/CustomDialogActivity.java
+++ app/src/main/java/com/fenglingtime/www/helloandroid/CustomDialogActivity.java
@@ -2,12 +2,36 @@ package com.fenglingtime.www.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.fenglingtime.www.helloandroid.widget.CustomDialog;
 
 public class CustomDialogActivity extends AppCompatActivity {
 
+    private Button mBtnDialog;
+    //private Button mBtnDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_dialog);
+        mBtnDialog=findViewById(R.id.btn_d1);
+        mBtnDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CustomDialog customDialog=new CustomDialog(CustomDialogActivity.this);
+                customDialog.setTitle("提示").setMsg("确认删除吗？").setCancel("取消",new CustomDialog.IOnCancelListener(){
+                    @Override
+                    public void onCancel(CustomDialog dialog) {
+
+                    }
+                }).setConfirm("确定", new CustomDialog.IOnConfirmListener() {
+                    @Override
+                    public void onConfirm(CustomDialog dialog) {
+
+                    }
+                }).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/fenglingtime/www/helloandroid/widget/CustomDialog.java
+++ app/src/main/java/com/fenglingtime/www/helloandroid/widget/CustomDialog.java
@@ -3,11 +3,49 @@ package com.fenglingtime.www.helloandroid.widget;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.text.method.ReplacementTransformationMethod;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
-public class CustomDialog extends Dialog {
+import com.fenglingtime.www.helloandroid.R;
+
+import javax.security.auth.PrivateCredentialPermission;
+
+public class CustomDialog extends Dialog implements View.OnClickListener {
+    private TextView mTvTitle, mTvMsg, mTvCancel, mTvConfirm;
+    private String title, msg, cancel, confirm;
+    private IOnCancelListener cancelListener;
+    private IOnConfirmListener confirmListener;
+
+    public CustomDialog setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public CustomDialog setMsg(String msg) {
+        this.msg = msg;
+        return this;
+    }
+
+    public CustomDialog setCancel(String cancel, IOnCancelListener listener) {
+        this.cancel = cancel;
+        this.cancelListener = listener;
+        return this;
+    }
+
+    public CustomDialog setConfirm(String confirm, IOnConfirmListener listener) {
+        this.confirm = confirm;
+        this.confirmListener = listener;
+        return this;
+    }
+
     public CustomDialog(@NonNull Context context) {
         super(context);
     }
@@ -19,6 +57,57 @@ public class CustomDialog extends Dialog {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_custom_dialog);
+        //设置宽度
+        WindowManager m = getWindow().getWindowManager();
+        Display d = m.getDefaultDisplay();
+        WindowManager.LayoutParams p = getWindow().getAttributes();
+        Point size = new Point();
+        d.getSize(size);
+        p.width = (int) (size.x * 0.8);
+        getWindow().setAttributes(p);
 
+        mTvTitle = findViewById(R.id.tv_title);
+        mTvMsg = findViewById(R.id.tv_msg);
+        mTvCancel = findViewById(R.id.tv_cancel);
+        mTvConfirm = findViewById(R.id.tv_confirm);
+        if (TextUtils.isEmpty(title)) {
+            mTvTitle.setText(title);
+        }
+        if (TextUtils.isEmpty(msg)) {
+            mTvMsg.setText(msg);
+        }
+        if (TextUtils.isEmpty(cancel)) {
+            mTvCancel.setText(cancel);
+        }
+        if (TextUtils.isEmpty(confirm)) {
+            mTvConfirm.setText(confirm);
+        }
+        mTvCancel.setOnClickListener(this);
+        mTvConfirm.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_cancel:
+                if (cancelListener != null) {
+                    cancelListener.onCancel(this);
+                }
+                break;
+            case R.id.tv_confirm:
+                if (confirmListener != null) {
+                    confirmListener.onConfirm(this);
+                }
+                break;
+        }
+    }
+
+    public interface IOnCancelListener {
+        void onCancel(CustomDialog dialog);
+    }
+
+    public interface IOnConfirmListener {
+        void onConfirm(CustomDialog dialog);
     }
 }
